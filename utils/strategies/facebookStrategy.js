5a321c75f70f1e29cab07ae69e1a904d5ff99359
Juan Jose Martinez Monsalve <juanmartinezmonsalve@gmail.com>
2022-04-13 03:55:57 -0500

    [FIX]Fixing FacebookStrategy's url

--- utils/strategies/facebookStrategy.js
+++ utils/strategies/facebookStrategy.js
@@ -1,11 +1,16 @@
 const {Strategy} = require('passport-facebook');
 
-
+let url = ''
+if(process.env.NODE_ENV === 'development'){
+  url = `http://${process.env.HOST}:${process.env.PORT}/auth/facebook/callback`
+}else{
+  url = `https://${process.env.HOST}/auth/facebook/callback`
+}
 const FacebookStrategy = new Strategy(
   {
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: `https://${process.env.HOST}:${process.env.PORT}/auth/facebook/callback`,
+    callbackURL: url,
   },
   function (token, tokenSecret, profile, done) {
     return done(null, profile);

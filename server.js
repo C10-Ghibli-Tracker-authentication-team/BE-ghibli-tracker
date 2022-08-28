afc3e1dfb0fd17be4a8bd926dc2b4dad4d31f30a
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-08-27 18:24:09 -0600

    🚧 Configure the middleware and protect API endpoints

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
-import express from 'express';
-import cors from 'cors';
-import { json, urlencoded } from 'body-parser';
-import passport from 'passport';
-import db from './db';
-import router from './network/routes';
-import './utils/strategies';
-
-require('dotenv').config();
-
-//db(`mongodb+srv://${process.env.MONGOATLAS_USER}:${process.env.MONGOATLAS_PASSWORD}@cluster0.v65cq.mongodb.net/${process.env.MONGOATLAS_APPNAME}?retryWrites=true&w=majority`);
-db(`mongodb+srv://${process.env.MONGOATLAS_USER}:${process.env.MONGOATLAS_PASSWORD}@cluster0.qoymy.mongodb.net/${process.env.MONGOATLAS_APPNAME}?retryWrites=true&w=majority`);
+const express = require('express');
 const app = express();
-app.use(json());
-app.use(cors());
-app.use(urlencoded({ extended: false }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 
-app.use(passport.initialize());
-app.use(passport.session());
-router(app);
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: 'https://ghiblitrackerapi.com',
+  issuerBaseURL: `https://fe-ghibli-tracker.us.auth0.com/`,
+});
 
-app.use('/app', express.static('public')); // servir estáticos desde la carpeta public
+// This route doesn't need authentication
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});
 
-app.listen(process.env.PORT);
-console.log(`La aplicacion esta escuchando en el puerto ${process.env.PORT}`);
+// This route needs authentication
+app.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+
+const checkScopes = requiredScopes('read:messages');
+
+app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Listening on http://localhost:3000');
+});
